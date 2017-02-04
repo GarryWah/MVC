bb54890b49d7b3904b3294dc4d634fe0d0cf06d2
GarryWah <dgarrywah@gmail.com>
2017-02-04 23:14:04 +0200

    Add files via upload

---
+++ fillDB.sql
@@ -0,0 +1,10 @@
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('20', 1, 'Grisha', '2016-03-03 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('21', 1, 'Ira', '2016-02-03 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('22', 1, 'Anya', '2016-04-03 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('23', 0, 'Oksana', '2016-05-03 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('24', 0, 'Misha', '2016-02-11 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('25', 0, 'Petya', '2016-02-10 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('26', 0, 'Vasya', '2016-05-09 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('27', 0, 'Andrey', '2016-02-09 00:00:00');
+INSERT INTO `test`.`user` (`age`, `isAdmin`, `name`, `createdDate`) VALUES ('28', 0, 'Vera.', '2016-02-07 00:00:00');
+
